ec9037e52926b2d7e4d72ce8ae3069bb0aec0a50
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-03-19 14:21:46 +0200

    ParseKind, ParseLogLevel.

--- ipc/marshaling/core.go
+++ ipc/marshaling/core.go
@@ -1,5 +1,7 @@
 package marshaling
 
+import "strings"
+
 // MarshalizerKind is the kind of a message (that is passed between the Node and Arwen)
 type MarshalizerKind uint32
 
@@ -9,6 +11,21 @@ const (
 	// Gob is a marshalizer kind
 	Gob
 )
+
+// ParseKind gets a kind from a string
+func ParseKind(str string) MarshalizerKind {
+	str = strings.ToUpper(str)
+	str = strings.Trim(str, " ")
+
+	switch str {
+	case "JSON":
+		return JSON
+	case "GOB":
+		return Gob
+	default:
+		return JSON
+	}
+}
 
 // Marshalizer deals with messages serialization
 type Marshalizer interface {
