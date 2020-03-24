fe348b5f32c9a21641f0e375deef073575335ba3
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-03-24 10:40:59 +0200

    Fix after review.

--- arwen/contexts/runtime.go
+++ arwen/contexts/runtime.go
@@ -2,7 +2,6 @@ package contexts
 
 import (
 	"fmt"
-	"time"
 	"unsafe"
 
 	"github.com/ElrondNetwork/arwen-wasm-vm/arwen"
@@ -61,8 +60,6 @@ func (context *runtimeContext) InitState() {
 }
 
 func (context *runtimeContext) CreateWasmerInstance(contract []byte, gasLimit uint64) error {
-	defer arwen.TimeTrack(time.Now(), "CreateWasmerInstance")
-
 	var err error
 	context.instance, err = wasmer.NewMeteredInstance(contract, gasLimit)
 	if err != nil {
