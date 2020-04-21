004922fb0e06eba6e5cfb88dd07ffd10bec8365e
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-21 15:14:20 +0300

    Sketch main.

--- cmd/arwendebug/main.go
+++ cmd/arwendebug/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/ElrondNetwork/arwen-wasm-vm/arwendebug"
+)
+
 func main() {
+	facade := &arwendebug.DebugFacade{}
+	app := arwendebug.Initialize(facade)
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
