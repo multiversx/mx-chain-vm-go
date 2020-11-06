a7a11c69f24321a7c9630a4a8e0248a086b0d70a
Robert Sasu <sasu.robert@gmail.com>
2020-11-06 17:05:48 +0200

    fix gasschedule message and response

--- ipc/common/messages.go
+++ ipc/common/messages.go
@@ -266,6 +266,7 @@ func IsStopRequest(message MessageHandler) bool {
 	return message.GetKind() == Stop
 }
 
+// IsVersionResponse returns version response
 func IsVersionResponse(message MessageHandler) bool {
 	return message.GetKind() == VersionResponse
 }
@@ -273,6 +274,11 @@ func IsVersionResponse(message MessageHandler) bool {
 // IsContractResponse returns whether a message is a contract response
 func IsContractResponse(message MessageHandler) bool {
 	return message.GetKind() == ContractResponse
+}
+
+// IsGasScheduleChangeResponse returns a message with gas schedule response
+func IsGasScheduleChangeResponse(message MessageHandler) bool {
+	return message.GetKind() == GasScheduleChangeResponse
 }
 
 // IsDiagnose returns whether a message is a diagnose request
