6bced6df1af3118979ccb14d1395a4c3e5363d62
dorin.iancu <dorin.iancu@elrond.com>
2021-02-09 16:02:15 +0200

    two async, same callback + storage rework + mandos tests fixes

--- test/contracts/promises/child-simple/src/lib.rs
+++ test/contracts/promises/child-simple/src/lib.rs
@@ -3,7 +3,7 @@
 #![allow(unused_attributes)]
 #![feature(lang_items)]
 
-use elrond_wasm::api::{EndpointFinishApi, ErrorApi};
+use elrond_wasm::api::{EndpointArgumentApi, EndpointFinishApi, ErrorApi};
 use elrond_wasm_node::ArwenApiImpl;
 
 pub static EEI: ArwenApiImpl = ArwenApiImpl{};
@@ -11,6 +11,21 @@ pub static EEI: ArwenApiImpl = ArwenApiImpl{};
 #[no_mangle]
 pub extern "C" fn answer() {
     EEI.finish_u64(42);
+}
+
+#[no_mangle]
+pub extern "C" fn answer_wrong() {
+    EEI.finish_u64(24);
+}
+
+// receives u64 as argument and returns it back
+#[no_mangle]
+pub extern "C" fn echo() {
+    EEI.check_num_arguments(1);
+
+    let arg = EEI.get_argument_u64(0);
+
+    EEI.finish_u64(arg);
 }
 
 #[no_mangle]
--- test/contracts/promises/parent-simple/src/lib.rs
+++ test/contracts/promises/parent-simple/src/lib.rs
@@ -3,7 +3,7 @@
 #![allow(unused_attributes)]
 #![feature(lang_items)]
 
-use elrond_wasm::api::{EndpointArgumentApi, EndpointFinishApi, StorageWriteApi};
+use elrond_wasm::api::{EndpointArgumentApi, EndpointFinishApi, StorageReadApi, StorageWriteApi};
 use elrond_wasm_node::ArwenApiImpl;
 
 use promises_common::*;
@@ -12,11 +12,14 @@ pub static EEI: ArwenApiImpl = ArwenApiImpl{};
 
 const SUCCESS_CALLBACK_ARGUMENT_KEY: &[u8] = b"SuccessCallbackArg";
 const FAIL_CALLBACK_ARGUMENT_KEY: &[u8] = b"FailCallbackArg";
+const CURRENT_STORAGE_INDEX_KEY: &[u8] = b"CurrentStorageIndex";
 
 #[no_mangle]
 pub extern "C" fn no_async() {
     EEI.finish_i64(42);
 }
+
+// one async call
 
 #[no_mangle]
 pub extern "C" fn one_async_call_no_cb_with_call_value() {
@@ -79,6 +82,26 @@ pub extern "C" fn one_async_call_fail_cb() {
                       100000);
 }
 
+// two async calls
+
+pub extern "C" fn two_async_same_cb_success_both() {
+    create_async_call("testgroup",
+                      &Address::from(CHILD_ADDRESS),
+                      &ZERO,
+                      b"echo@0x01",
+                      "success_callback_one_arg",
+                      "fail_callback",
+                      100000);
+    
+    create_async_call("testgroup",
+                      &Address::from(CHILD_ADDRESS),
+                      &ZERO,
+                      b"echo@0x02",
+                      "success_callback_one_arg",
+                      "fail_callback",
+                      100000);
+}
+
 // callbacks
 
 // first argument is "0" for success, followed by data passed by finish() in callee contract
@@ -87,12 +110,17 @@ pub extern "C" fn success_callback_one_arg() {
     let expected_num_args = 2;
     EEI.check_num_arguments(expected_num_args);
 
+    let mut storage_index = EEI.storage_load_u64(&CURRENT_STORAGE_INDEX_KEY);
+
     for arg_index in 0..expected_num_args {
         let arg = EEI.get_argument_u64(arg_index);
-        let storage_key = construct_storage_key(&[SUCCESS_CALLBACK_ARGUMENT_KEY, &[arg_index as u8]]);
-    
+        let storage_key = construct_storage_key(&[SUCCESS_CALLBACK_ARGUMENT_KEY, &[storage_index as u8]]);
+
+        storage_index += 1;
         EEI.storage_store_u64(&storage_key, arg);
     }
+
+    EEI.storage_store_u64(&CURRENT_STORAGE_INDEX_KEY, storage_index);
 }
 
 // first argument is error code, followed by error message
@@ -101,10 +129,16 @@ pub extern "C" fn fail_callback() {
     let expected_num_args = 2;
     EEI.check_num_arguments(expected_num_args);
 
+    let mut storage_index = EEI.storage_load_u64(&CURRENT_STORAGE_INDEX_KEY);
+
     for arg_index in 0..expected_num_args {
         let arg = EEI.get_argument_vec_u8(arg_index);
-        let storage_key = construct_storage_key(&[FAIL_CALLBACK_ARGUMENT_KEY, &[arg_index as u8]]);
+        let storage_key = construct_storage_key(&[FAIL_CALLBACK_ARGUMENT_KEY, &[storage_index as u8]]);
     
+        storage_index += 1;
         EEI.storage_store_slice_u8(&storage_key, &arg);
     }
+
+    EEI.storage_store_u64(&CURRENT_STORAGE_INDEX_KEY, storage_index);
 }
+
