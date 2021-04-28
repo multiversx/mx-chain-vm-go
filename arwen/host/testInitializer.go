224e2799e24c6df4dc7387b5dd771d3c39016e38
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-04-28 16:20:55 +0300

    Gas fix for local async callbacks

--- arwen/host/testInitializer.go
+++ arwen/host/testInitializer.go
@@ -71,7 +71,7 @@ func GetTestSCCodeModule(scName string, moduleName string, prefixToTestSCs strin
 // BuildSCModule invokes erdpy to build the contract into a WASM module
 func BuildSCModule(scName string, prefixToTestSCs string) {
 	pathToSCDir := prefixToTestSCs + "test/contracts/" + scName
-	out, err := exec.Command("erdpy", "contract", "build", pathToSCDir).Output()
+	out, err := exec.Command("erdpy", "contract", "build", "--no-optimization", pathToSCDir).Output()
 	if err != nil {
 		log.Error("error building contract", "err", err, "contract", pathToSCDir)
 		return
