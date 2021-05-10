a9b81367fa262162df1cdf1082f0bb2602fb1cfa
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-05-10 17:16:59 +0300

    added interface for elliptic curves

--- crypto/elliptic_curves/elliptic_curves.go
+++ crypto/elliptic_curves/elliptic_curves.go
@@ -2,6 +2,7 @@ package elliptic_curves
 
 import (
 	"crypto/elliptic"
+	"crypto/rand"
 	"math/big"
 )
 
@@ -38,4 +39,29 @@ func ScalarBaseMult(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int
 func ScalarMult(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int, Bx *big.Int, By *big.Int, k []byte) (*big.Int, *big.Int) {
 	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
 	return myEc.curve.ScalarMult(Bx, By, k)
+}
+
+func Marshal(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int, x *big.Int, y *big.Int) []byte {
+	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
+	return elliptic.Marshal(&myEc.curve, x, y)
+}
+
+func Unmarshal(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int, data []byte) (*big.Int, *big.Int) {
+	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
+	return elliptic.Unmarshal(&myEc.curve, data)
+}
+
+func GenerateKey(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int) (priv []byte, x, y *big.Int, err error) {
+	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
+	return elliptic.GenerateKey(&myEc.curve, rand.Reader)
+}
+
+func MarshalCompressed(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int, x *big.Int, y *big.Int) []byte {
+	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
+	return elliptic.MarshalCompressed(&myEc.curve, x, y)
+}
+
+func UnmarshalCompressed(P *big.Int, N *big.Int, B *big.Int, Gx *big.Int, Gy *big.Int, BitSize int, data []byte) (*big.Int, *big.Int) {
+	myEc := NewEllipticCurve(P, N, B, Gx, Gy, BitSize)
+	return elliptic.UnmarshalCompressed(&myEc.curve, data)
 }
