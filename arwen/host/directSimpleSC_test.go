013aa17f52a0c0988e7724fd3ffff47cc639cb3b
matei-p <bogdandanu@gmail.com>
2021-05-11 20:48:12 +0300

    fixes after review

--- arwen/host/directSimpleSC_test.go
+++ arwen/host/directSimpleSC_test.go
@@ -30,17 +30,26 @@ func execOnSameCtxParentMock(instanceMock *mock.InstanceMock, config interface{}
 		t := instance.T
 		host.Metering().UseGas(testConfig.gasUsedByParent)
 
+		argsPerCall := 3
 		arguments := host.Runtime().Arguments()
+		if len(arguments)%argsPerCall != 0 {
+			host.Runtime().SignalUserError("need 3 arguments per individual call")
+			return instance
+		}
+
 		input := DefaultTestContractCallInput()
 		input.GasProvided = testConfig.gasProvidedToChild
 		input.CallerAddr = instance.Address
-		input.RecipientAddr = arguments[0]
-		input.Function = string(arguments[1])
-		numCalls := big.NewInt(0).SetBytes(arguments[2]).Uint64()
 
-		for i := uint64(0); i < numCalls; i++ {
-			_, err := host.ExecuteOnSameContext(input)
-			require.Nil(t, err)
+		for callIndex := 0; callIndex < len(arguments); callIndex += argsPerCall {
+			input.RecipientAddr = arguments[callIndex+0]
+			input.Function = string(arguments[callIndex+1])
+			numCalls := big.NewInt(0).SetBytes(arguments[callIndex+2]).Uint64()
+
+			for i := uint64(0); i < numCalls; i++ {
+				_, err := host.ExecuteOnSameContext(input)
+				require.Nil(t, err)
+			}
 		}
 
 		return instance
