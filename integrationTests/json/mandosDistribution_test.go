396a87f4b106fc2b3eeaa8872546372dde51fea9
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-05-18 13:58:14 +0300

    wip on double apr tests

--- integrationTests/json/mandosDistribution_test.go
+++ integrationTests/json/mandosDistribution_test.go
@@ -11,3 +11,11 @@ func TestDistribution_v0_1(t *testing.T) {
 
 	runAllTestsInFolder(t, "distribution/v0_1")
 }
+
+func TestDistribution_v0_1_single(t *testing.T) {
+	if testing.Short() {
+		t.Skip("not a short test")
+	}
+
+	runSingleTest(t, "distribution/v0_1/mandos", "claim_rewards_proxy_after_enter_with_lock_after_mint_rewards.scen.json")
+}
