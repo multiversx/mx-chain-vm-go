d7301ea377bc6b48c95ff034a9c6c92cf8d37f19
andrei-marinica <andrei.marinica@elrond.com>
2021-05-24 06:04:27 +0300

    arwenmandos - gas schedules embedded in binary

--- arwen/hosttest/execution_benchmark_test.go
+++ arwen/hosttest/execution_benchmark_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ElrondNetwork/arwen-wasm-vm/arwen"
 	arwenHost "github.com/ElrondNetwork/arwen-wasm-vm/arwen/host"
+	gasSchedules "github.com/ElrondNetwork/arwen-wasm-vm/arwenmandos/gasSchedules"
 	worldmock "github.com/ElrondNetwork/arwen-wasm-vm/mock/world"
 	testcommon "github.com/ElrondNetwork/arwen-wasm-vm/testcommon"
 	"github.com/ElrondNetwork/elrond-go/core/vmcommon"
@@ -84,7 +85,7 @@ func deploy(tb testing.TB, totalTokenSupply *big.Int) (arwen.VMHost, *worldmock.
 		NewAddress:     scAddress,
 	})
 
-	gasMap, err := testcommon.LoadGasScheduleConfig("../../arwenmandos/gasSchedules/gasScheduleV2.toml")
+	gasMap, err := testcommon.LoadGasScheduleConfig(gasSchedules.GetV2())
 	require.Nil(tb, err)
 
 	host, err := arwenHost.NewArwenVM(mockWorld, &arwen.VMHostParameters{
