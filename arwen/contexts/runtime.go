53233e531a9c85583e372ae36935719bf417ecf7
matei-p <bogdandanu@gmail.com>
2021-06-08 15:00:42 +0300

    cosmetic fix

--- arwen/contexts/runtime.go
+++ arwen/contexts/runtime.go
@@ -518,7 +518,7 @@ func (context *runtimeContext) FailExecution(err error) {
 
 	var message string
 	if err != nil {
-		wrappedError, ok := (err.(arwen.WrappableError))
+		wrappedError, ok := err.(arwen.WrappableError)
 		if ok {
 			message = wrappedError.GetLastError().Error()
 		} else {
