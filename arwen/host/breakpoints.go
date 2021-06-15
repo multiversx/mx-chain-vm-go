dd171f5539fd4aefa80f38436bebe84306a74841
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-06-15 16:40:11 +0300

    Revert "Revert changes to breakpoints.go"
    This reverts commit 2a76c3b789f371781b2cb6a591fdc1764a1dba53.

--- arwen/host/breakpoints.go
+++ arwen/host/breakpoints.go
@@ -10,12 +10,13 @@ func (host *vmHost) handleBreakpointIfAny(executionErr error) error {
 	}
 
 	runtime := host.Runtime()
+	runtime.AddError(executionErr)
+
 	breakpointValue := runtime.GetRuntimeBreakpointValue()
 	if breakpointValue != arwen.BreakpointNone {
-		if breakpointValue != arwen.BreakpointAsyncCall {
-			runtime.AddError(executionErr, runtime.Function())
-		}
-		return host.handleBreakpoint(breakpointValue)
+		err := host.handleBreakpoint(breakpointValue)
+		runtime.AddError(err)
+		return err
 	}
 
 	return executionErr
