102993885d28d3c6cce1b26640730bda26619797
dorin.iancu <dorin.iancu@elrond.com>
2021-06-17 16:07:42 +0300

    mimic Action enum + basic test main func

---
+++ fuzz/elrond-ethereum-bridge/fuzzElrondEthereumBridgeAction.go
@@ -0,0 +1,29 @@
+package elrond_ethereum_bridge
+
+type MultisigAction int
+
+const (
+	ActionNone MultisigAction = iota
+	ActionAddBoardMember
+	ActionAddProposer
+	ActionRemoveUser
+	ActionSlashUser
+	ActionChangeQuorum
+	ActionSetCurrentTransactionBatchStatus
+	ActionBatchTransferEsdtToken
+)
+
+type Action struct {
+	actionType MultisigAction
+	data       interface{}
+}
+
+type SetCurrentTransactionBatchStatusActionData struct {
+	relayerRewardAddress   Address
+	transactionBatchStatus []TransactionStatus
+}
+
+type BatchTransferEsdtTokenActionData struct {
+	batchId   int
+	transfers []*SimpleTransfer
+}
---
+++ fuzz/elrond-ethereum-bridge/fuzzElrondEthereumBridge_test.go
@@ -0,0 +1,66 @@
+package elrond_ethereum_bridge
+
+import (
+	"flag"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	fuzzutil "github.com/ElrondNetwork/arwen-wasm-vm/v1_3/fuzz/util"
+	mc "github.com/ElrondNetwork/arwen-wasm-vm/v1_3/mandos-go/controller"
+)
+
+var fuzz = flag.Bool("fuzz", true, "Enable fuzz test")
+
+var seedFlag = flag.Int64("seed", 0, "Random seed, use it to replay fuzz scenarios")
+
+var iterationsFlag = flag.Int("iterations", 1000, "Number of iterations")
+
+func getTestRoot() string {
+	exePath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	arwenTestRoot := filepath.Join(exePath, "../../../test")
+	return arwenTestRoot
+}
+
+func newExecutorWithPaths() *fuzzExecutor {
+	fileResolver := mc.NewDefaultFileResolver().
+		ReplacePath(
+			"price-aggregator.wasm",
+			filepath.Join(getTestRoot(), "elrond-ethereum-bridge/price-aggregator/price-aggregator.wasm"))
+
+	fe, err := newFuzzExecutor(fileResolver)
+	if err != nil {
+		panic(err)
+	}
+	return fe
+}
+
+func TestElrondEthereumBridge(t *testing.T) {
+	if !*fuzz {
+		t.Skip("skipping test; only run with --fuzz argument")
+	}
+
+	fe := newExecutorWithPaths()
+	defer fe.saveGeneratedScenario()
+
+	var seed int64
+	if *seedFlag == 0 {
+		seed = time.Now().UnixNano()
+	} else {
+		seed = *seedFlag
+	}
+	fe.log("Random seed: %d\n", seed)
+	r := rand.New(rand.NewSource(seed))
+	r.Seed(seed)
+
+	re := fuzzutil.NewRandomEventProvider(r)
+	re.Reset()
+
+	fe.initData()
+	fe.setupAggregator()
+}
