b52525034ff7eca84eb9702061b45f625a4b264f
matei-p <bogdandanu@gmail.com>
2021-07-30 00:27:18 +0300

    moved graph tests

---
+++ arwen/hosttest/execution_graph_test.go
@@ -0,0 +1,122 @@
+package hosttest
+
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/arwen-wasm-vm/v1_4/arwen"
+	worldmock "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/mock/world"
+	test "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/testcommon"
+)
+
+func TestGasUsed_OneAsyncCall_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestOneAsyncCall()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_TwoAsyncCalls_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestTwoAsyncCalls()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_AsyncCallsAsync_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestAsyncCallsAsync()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_CallbackCallsAsync_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestCallbackCallsAsync()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_GroupCallbacks_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestGroupCallbacks()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_SimpleSyncAndAsync1_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestSimpleSyncAndAsync1()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_SimpleSyncAndAsync2_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTestSimpleSyncAndAsync2()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_GraphTest1_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTest1()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+func TestGasUsed_GraphTest2_CallGraph(t *testing.T) {
+	callGraph := test.CreateGraphTest2()
+	runGraphCallTestTemplate(t, callGraph)
+}
+
+type usedGasPerContract struct {
+	contractAddress []byte
+	gasUsed         uint64
+}
+
+func runGraphCallTestTemplate(t *testing.T, callGraph *test.TestCallGraph) {
+	testConfig := makeTestConfig()
+	testConfig.GasProvided = callGraph.StartNode.GasLimit
+	testConfig.GasLockCost = 0
+
+	// compute execution order (return data) assertions
+	expectedReturnData := make([][]byte, 0)
+	executionGraph := callGraph.CreateExecutionGraphFromCallGraph()
+	startNode := executionGraph.GetStartNode()
+
+	executionOrder := test.CreateRunOrderFromExecutionGraph(executionGraph)
+	for _, testCall := range executionOrder {
+		expectedReturnData = append(expectedReturnData, []byte(string(testCall.ContractAddress)+"_"+testCall.FunctionName+test.TestReturnDataSuffix))
+	}
+
+	// compute gas assertions
+	gasGraph := executionGraph.CreateGasGraphFromExecutionGraph()
+	gasGraph.ComputeRemainingGasBeforeCallbacks()
+	gasGraph.ComputeGasStepByStep(func(graph *test.TestCallGraph, step int) {})
+
+	totalGasUsed := uint64(0)
+	expectedGasUsagePerContract := make(map[string]*usedGasPerContract)
+	gasGraph.DfsGraph(func(path []*test.TestCallNode, parent *test.TestCallNode, node *test.TestCallNode, incomingEdge *test.TestCallEdge) *test.TestCallNode {
+		if !node.IsLeaf() {
+			return node
+		}
+		gasPerContract := expectedGasUsagePerContract[string(parent.Call.ContractAddress)]
+		if gasPerContract == nil {
+			gasPerContract = &usedGasPerContract{
+				contractAddress: parent.Call.ContractAddress,
+				gasUsed:         0,
+			}
+			expectedGasUsagePerContract[string(parent.Call.ContractAddress)] = gasPerContract
+		}
+		gasPerContract.gasUsed += node.GasUsed
+		totalGasUsed += node.GasUsed
+		return node
+	})
+
+	test.BuildMockInstanceCallTest(t).
+		WithContracts(
+			test.CreateMockContractsFromAsyncTestCallGraph(callGraph, testConfig)...,
+		).
+		WithInput(test.CreateTestContractCallInputBuilder().
+			WithRecipientAddr([]byte(startNode.Call.ContractAddress)).
+			WithGasProvided(testConfig.GasProvided).
+			WithFunction(startNode.Call.FunctionName).
+			Build()).
+		WithSetup(func(host arwen.VMHost, world *worldmock.MockWorld) {
+			setZeroCodeCosts(host)
+			setAsyncCosts(host, testConfig.GasLockCost)
+		}).
+		AndAssertResults(func(world *worldmock.MockWorld, verify *test.VMOutputVerifier) {
+			verifier := verify.
+				Ok().
+				ReturnDataForGraphTesting(expectedReturnData...).
+				GasRemaining(callGraph.StartNode.GasLimit - totalGasUsed)
+			for _, gasPerContract := range expectedGasUsagePerContract {
+				verifier.GasUsed(gasPerContract.contractAddress, gasPerContract.gasUsed)
+			}
+		})
+}
