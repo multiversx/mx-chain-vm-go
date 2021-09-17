8d57defe368fc09bf8b010f325c43e82a167ec86
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-09-17 13:04:40 +0300

    create big float op wrapper to avoid panics using defer-recover

--- math/bigFloat_wrapper.go
+++ math/bigFloat_wrapper.go
@@ -5,14 +5,62 @@ import (
 	"math/big"
 )
 
-func Sub(result, op1, op2 *big.Float) error {
-	var err error
+func SubBigFloat(op1, op2 *big.Float) (result *big.Float, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("%w while doing float.Sub", ErrOperationCausingPanic)
-			result = big.NewFloat(0)
+			result.SetFloat64(0)
 		}
 	}()
+	result = new(big.Float)
 	result.Sub(op1, op2)
-	return err
+	return
+}
+
+func AddBigFloat(op1, op2 *big.Float) (result *big.Float, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%w while doing float.Add", ErrOperationCausingPanic)
+			result.SetFloat64(0)
+		}
+	}()
+	result = new(big.Float)
+	result.Add(op1, op2)
+	return
+}
+
+func QuoBigFloat(op1, op2 *big.Float) (result *big.Float, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%w while doing float.Quo", ErrOperationCausingPanic)
+			result.SetFloat64(0)
+		}
+	}()
+	result = new(big.Float)
+	result.Quo(op1, op2)
+	return
+}
+
+func MulBigFloat(op1, op2 *big.Float) (result *big.Float, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%w while doing float.Mul", ErrOperationCausingPanic)
+			result.SetFloat64(0)
+		}
+	}()
+	result = new(big.Float)
+	result.Mul(op1, op2)
+	return
+}
+
+func SqrtBigFloat(op *big.Float) (result *big.Float, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%w while doing float.Sqrt", ErrOperationCausingPanic)
+			result.SetFloat64(0)
+		}
+	}()
+	result = new(big.Float)
+	result.Sqrt(op)
+	return
 }
