86cf40febc888d7f71741292158fd86786bc8c2a
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-10-19 14:20:32 +0300

    tests with/without external steps

--- arwenmandos/elrondgo-exporter/mandosTests/adder_exporter_test.go
+++ arwenmandos/elrondgo-exporter/mandosTests/adder_exporter_test.go
@@ -1,9 +1,10 @@
 package mandosTests
 
 import (
-	"github.com/ElrondNetwork/arwen-wasm-vm/v1_4/arwenmandos/elrondgo-exporter"
 	"math/big"
 	"testing"
+
+	elrondgo_exporter "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/arwenmandos/elrondgo-exporter"
 
 	"github.com/ElrondNetwork/arwen-wasm-vm/v1_4/arwen"
 	mj "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/mandos-go/model"
@@ -11,12 +12,18 @@ import (
 )
 
 // address:owner
-var addressOwner = []byte{111, 119, 110, 101, 114, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95}
+var addressOwner = []byte("owner___________________________")
 
 // address:adder
-var addressAdder = []byte{97, 100, 100, 101, 114, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95}
+var addressAdder = []byte("adder___________________________")
 
-func TestGetAccountsAndTransactionsFromAdder(t *testing.T) {
+// address:alice
+var addressAlice = []byte("alice___________________________")
+
+// address:bob
+var addressBob = []byte("bob_____________________________")
+
+func TestGetAccountsAndTransactionsFrom_Adder(t *testing.T) {
 	accounts, transactions, err := elrondgo_exporter.GetAccountsAndTransactionsFromMandos("adder.scen.json")
 	require.Nil(t, err)
 	expectedAccs := make([]*elrondgo_exporter.TestAccount, 0)
@@ -25,12 +32,31 @@ func TestGetAccountsAndTransactionsFromAdder(t *testing.T) {
 	ownerAccount := elrondgo_exporter.SetNewAccount(1, addressOwner, big.NewInt(48), make(map[string][]byte), make([]byte, 0), make([]byte, 0))
 	scAccount := elrondgo_exporter.SetNewAccount(0, addressAdder, big.NewInt(0), make(map[string][]byte), arwen.GetSCCode("../../../test/adder/output/adder.wasm"), addressOwner)
 	expectedAccs = append(expectedAccs, ownerAccount, scAccount)
-	require.Equal(t, 2, len(expectedAccs))
 
 	transaction := elrondgo_exporter.CreateTransaction("add", [][]byte{{3}}, 0, big.NewInt(0), make([]*mj.ESDTTxData, 0), accounts[0].GetAddress(), accounts[1].GetAddress(), 5000000, 0)
 	expectedTxs = append(expectedTxs, transaction)
 
 	require.Nil(t, err)
 	require.Equal(t, expectedAccs, accounts)
+	require.Equal(t, expectedTxs, transactions)
+}
+
+func TestGetAccountsAndTransactionsFrom_AdderWithExternalSteps(t *testing.T) {
+	accounts, transactions, err := elrondgo_exporter.GetAccountsAndTransactionsFromMandos("adder_with_external_steps.scen.json")
+	require.Nil(t, err)
+	expectedAccs := make([]*elrondgo_exporter.TestAccount, 0)
+	expectedTxs := make([]*elrondgo_exporter.Transaction, 0)
+
+	ownerAccount := elrondgo_exporter.SetNewAccount(1, addressOwner, big.NewInt(48), make(map[string][]byte), make([]byte, 0), make([]byte, 0))
+	scAccount := elrondgo_exporter.SetNewAccount(0, addressAdder, big.NewInt(0), make(map[string][]byte), arwen.GetSCCode("../../../test/adder/output/adder.wasm"), addressOwner)
+	aliceAccount := elrondgo_exporter.SetNewAccount(5, addressAlice, big.NewInt(284), make(map[string][]byte), make([]byte, 0), make([]byte, 0))
+	bobAccount := elrondgo_exporter.SetNewAccount(3, addressBob, big.NewInt(11), make(map[string][]byte), make([]byte, 0), make([]byte, 0))
+	expectedAccs = append(expectedAccs, aliceAccount, scAccount, bobAccount, ownerAccount)
+	require.Equal(t, expectedAccs, accounts)
+
+	transactionAlice := elrondgo_exporter.CreateTransaction("add", [][]byte{{3}}, 0, big.NewInt(0), make([]*mj.ESDTTxData, 0), accounts[0].GetAddress(), accounts[1].GetAddress(), 5000000, 0)
+	transactionBob := elrondgo_exporter.CreateTransaction("add", [][]byte{{3}}, 0, big.NewInt(0), make([]*mj.ESDTTxData, 0), accounts[2].GetAddress(), accounts[1].GetAddress(), 5000000, 0)
+	transactionOwner := elrondgo_exporter.CreateTransaction("add", [][]byte{{3}}, 0, big.NewInt(0), make([]*mj.ESDTTxData, 0), accounts[3].GetAddress(), accounts[1].GetAddress(), 5000000, 0)
+	expectedTxs = append(expectedTxs, transactionBob, transactionAlice, transactionOwner)
 	require.Equal(t, expectedTxs, transactions)
 }
