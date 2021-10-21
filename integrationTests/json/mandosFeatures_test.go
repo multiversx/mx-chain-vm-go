524818d2c9167621a47150a81eadb397fdbe80ae
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-10-21 13:09:43 +0300

    Exclude two test scenarios that shouldn't work anyway

--- integrationTests/json/mandosFeatures_test.go
+++ integrationTests/json/mandosFeatures_test.go
@@ -2,8 +2,6 @@ package vmjsonintegrationtest
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestRustBasicFeaturesLatest(t *testing.T) {
@@ -40,16 +38,25 @@ func TestRustPayableFeaturesLatest(t *testing.T) {
 }
 
 func TestRustComposability(t *testing.T) {
-	runAllTestsInFolder(t, "features/composability/mandos")
+	// TODO The two excluded tests perform async calls from within async calls,
+	// which are unsupported by the legacy async calls on which the forwarder is
+	// currently based. The new AsyncContext will block multi-level async calls
+	// anyway in its first release.
+	runTestsInFolder(t, "features/composability/mandos", []string{
+		"features/composability/mandos/forwarder_send_twice_egld.scen.json",
+		"features/composability/mandos/forwarder_send_twice_esdt.scen.json",
+	})
 }
 
 func TestRustLegacyComposability(t *testing.T) {
-	runAllTestsInFolder(t, "features/composability/mandos-legacy")
-}
-
-func TestSingleScenario(t *testing.T) {
-	err := runSingleTestReturnError("features/composability/mandos", "forwarder_send_twice_egld.scen.json")
-	require.Nil(t, err)
+	// TODO The two excluded tests perform async calls from within async calls,
+	// which are unsupported by the legacy async calls on which the forwarder is
+	// currently based. The new AsyncContext will block multi-level async calls
+	// anyway in its first release.
+	runTestsInFolder(t, "features/composability/mandos-legacy", []string{
+		"features/composability/mandos-legacy/l_forwarder_send_twice_egld.scen.json",
+		"features/composability/mandos-legacy/l_forwarder_send_twice_esdt.scen.json",
+	})
 }
 
 func TestTimelocks(t *testing.T) {
