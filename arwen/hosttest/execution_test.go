5c4d01cde6c67ce6f2d8efb6a162e0e41e657e68
matei-p <bogdandanu@gmail.com>
2021-11-12 15:18:36 +0200

    test fix

--- arwen/hosttest/execution_test.go
+++ arwen/hosttest/execution_test.go
@@ -2531,12 +2531,12 @@ func TestExecution_AsyncCall_CallBackFails(t *testing.T) {
 				Ok().
 				// TODO matei-p enable this for R2
 				//UserError().
-				// ReturnMessage("callBack error").
-				GasUsed(test.ParentAddress, 198674).
+				//ReturnMessage("callBack error").
+				GasUsed(test.ParentAddress, 198390).
 				GasUsed(test.ChildAddress, 1297).
 				// TODO Why is there a minuscule amount of gas remaining after the callback
 				// fails? This is supposed to be 0.
-				GasRemaining(190).
+				GasRemaining(313).
 				BalanceDelta(test.ThirdPartyAddress, 6).
 				BalanceDelta(test.ChildAddress, big.NewInt(0).Sub(big.NewInt(1), big.NewInt(1)).Int64()).
 				ReturnData(test.ParentFinishA, test.ParentFinishB, []byte{3}, []byte("thirdparty"), []byte("vault"), []byte("user error")).
