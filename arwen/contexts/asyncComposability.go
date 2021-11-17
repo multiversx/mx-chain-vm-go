bcb132ee3d454787981a61fab56d5a9330174d71
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-11-17 16:06:53 +0200

    Split NotifyChildIsComplete()

--- arwen/contexts/asyncComposability.go
+++ arwen/contexts/asyncComposability.go
@@ -19,47 +19,10 @@ func (context *asyncContext) NotifyChildIsComplete(callID []byte, gasToAccumulat
 	context.completeChild(callID, gasToAccumulate)
 
 	if !context.IsComplete() {
-		// store changes in context made by CompleteChild()
-		err := context.Save()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		// There are no more callbacks to return from other shards. The context can
-		// be deleted from storage.
-		err := context.DeleteFromAddress(context.address)
-		// err := context.Delete()
-		if err != nil {
-			return nil, err
-		}
-
-		// if we reached first call, stop notification chain
-		if context.IsFirstCall() {
-			return context, nil
-		}
-
-		currentCallID := context.GetCallID()
-		gasAccumulatedInNotifingContext := context.gasAccumulated
-		if context.callType == vm.AsynchronousCall {
-			vmOutput := context.childResults
-			isComplete, _, err := context.callCallback(currentCallID, vmOutput, nil)
-			if err != nil {
-				return nil, err
-			}
-			if isComplete {
-				return context.NotifyChildIsComplete(currentCallID, 0)
-			}
-		} else if context.callType == vm.AsynchronousCallBack {
-			currentCallID := context.GetCallerCallID()
-			context.LoadParentContext()
-			return context.NotifyChildIsComplete(currentCallID, gasAccumulatedInNotifingContext)
-		} else if context.callType == vm.DirectCall {
-			context.LoadParentContext()
-			return context.NotifyChildIsComplete(nil, gasAccumulatedInNotifingContext)
-		}
+		return context.Save()
 	}
 
-	return context, nil
+	return context.complete()
 }
 
 func (context *asyncContext) completeChild(callID []byte, gasToAccumulate uint64) error {
@@ -81,23 +44,37 @@ func (context *asyncContext) CompleteChildConditional(isComplete bool, callID []
 	return nil
 }
 
-func (context *asyncContext) removeAsyncCallIfCompleted(
-	callID []byte,
-	returnCode vmcommon.ReturnCode,
-) error {
-	asyncCall, _, _, err := context.GetAsyncCallByCallID(callID)
+func (context *asyncContext) complete() error {
+	// There are no more callbacks to return from other shards. The context can
+	// be deleted from storage.
+	err := context.DeleteFromAddress(context.address)
 	if err != nil {
 		return err
 	}
-	// The vmOutput instance returned by host.ExecuteOnDestContext() is never nil,
-	// by design. Using it without checking for err is safe here.
-	asyncCall.UpdateStatus(returnCode)
 
-	context.closeCompletedAsyncCalls()
-	if context.groupCallbacksEnabled {
-		context.executeCompletedGroupCallbacks()
+	// if we reached first call, stop notification chain
+	if context.IsFirstCall() {
+		return nil
 	}
-	context.deleteCompletedGroups()
+
+	currentCallID := context.GetCallID()
+	if context.callType == vm.AsynchronousCall {
+		vmOutput := context.childResults
+		isComplete, _, err := context.callCallback(currentCallID, vmOutput, nil)
+		if err != nil {
+			return err
+		}
+		if isComplete {
+			return context.NotifyChildIsComplete(currentCallID, 0)
+		}
+	} else if context.callType == vm.AsynchronousCallBack {
+		currentCallID := context.GetCallerCallID()
+		context.LoadParentContext()
+		return context.NotifyChildIsComplete(currentCallID, context.gasAccumulated)
+	} else if context.callType == vm.DirectCall {
+		context.LoadParentContext()
+		return context.NotifyChildIsComplete(nil, context.gasAccumulated)
+	}
 
 	return nil
 }
