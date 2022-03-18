9c778cfec906c2d885f5ae27e5a4d9e0c4762db1
Robert Sasu <sasu.robert@gmail.com>
2022-03-18 10:53:08 +0200

    false everywhere

--- testcommon/testInitializer_inputs.go
+++ testcommon/testInitializer_inputs.go
@@ -327,7 +327,7 @@ func DefaultTestArwenWithWorldMockWithGasSchedule(tb testing.TB, customGasSchedu
 // DefaultTestArwen creates a host configured with a configured blockchain hook
 func DefaultTestArwen(tb testing.TB, blockchain vmcommon.BlockchainHook) arwen.VMHost {
 	customGasSchedule := config.GasScheduleMap(nil)
-	return DefaultTestArwenWithGasSchedule(tb, blockchain, customGasSchedule, true)
+	return DefaultTestArwenWithGasSchedule(tb, blockchain, customGasSchedule, false)
 }
 
 func DefaultTestArwenWithGasSchedule(
