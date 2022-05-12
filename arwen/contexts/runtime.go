e2a5c71ffbeffdadb1b8e39afddaa7e45c1436b2
robertsasu <sasu.robert@gmail.com>
2022-05-12 12:52:23 +0300

    added backward compatibility for big floats functions

--- arwen/contexts/runtime.go
+++ arwen/contexts/runtime.go
@@ -776,6 +776,75 @@ func (context *runtimeContext) checkIfContainsNewManagedCryptoAPI() error {
 	if context.instance.IsFunctionImported("managedCreateEC") {
 		return arwen.ErrContractInvalid
 	}
+	if context.instance.IsFunctionImported("mBufferToBigFloat") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("mBufferFromBigFloat") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatNewFromParts") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatNewFromFrac") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatNewFromSci") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatAdd") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatSub") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatMul") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatDiv") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatAbs") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatCmp") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatSign") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatClone") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatSqrt") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatPow") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatFloor") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatCeil") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatTruncate") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatIsInt") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatSetInt64") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatSetBigInt") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatGetConstPi") {
+		return arwen.ErrContractInvalid
+	}
+	if context.instance.IsFunctionImported("bigFloatGetConstE") {
+		return arwen.ErrContractInvalid
+	}
 
 	return nil
 }
