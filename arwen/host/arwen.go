0f2a01778d79926547f64f241b65ab93f2fc72b8
matei-p <bogdandanu@gmail.com>
2022-06-07 17:01:57 +0300

    test 1

--- arwen/host/arwen.go
+++ arwen/host/arwen.go
@@ -29,7 +29,7 @@ var MaximumWasmerInstanceCount = uint64(10)
 
 var _ arwen.VMHost = (*vmHost)(nil)
 
-const minExecutionTimeout = 3600 * time.Second
+const minExecutionTimeout = time.Second
 const internalVMErrors = "internalVMErrors"
 
 // vmHost implements HostContext interface.
