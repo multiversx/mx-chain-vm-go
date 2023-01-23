873da184c406033ae853a63b588f48909cd03e43
Ovidiu Stinga <ovstinga@gmail.com>
2023-01-23 17:51:42 +0200

    comment tests again

--- integrationTests/json/mandosExecutorLogs_test.go
+++ integrationTests/json/mandosExecutorLogs_test.go
@@ -151,7 +151,7 @@ func TestESDTMultiTransferOnCallAndCallbackLog(t *testing.T) {
 }
 
 func TestMultisigLog(t *testing.T) {
-	// t.Skip("not a working test")
+	t.Skip("not a working test")
 
 	expected := MandosTest(t).
 		Folder("multisig/scenarios").
@@ -170,7 +170,7 @@ func TestMultisigLog(t *testing.T) {
 }
 
 func TestDnsContractLog(t *testing.T) {
-	// t.Skip("not a working test")
+	t.Skip("not a working test")
 
 	if testing.Short() {
 		t.Skip("not a short test")
