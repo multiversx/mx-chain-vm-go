85408cf2f4c747df3d1b1b2d903ce491ec8c6f5d
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2023-02-16 15:20:06 +0200

    Remove RuntimeContext.SCCodeSize()

--- vmhost/interface.go
+++ vmhost/interface.go
@@ -115,7 +115,6 @@ type RuntimeContext interface {
 	GetContextAddress() []byte
 	SetCodeAddress(scAddress []byte)
 	GetSCCode() ([]byte, error)
-	SetSCCodeSize(size uint64)
 	GetSCCodeSize() uint64
 	GetVMType() []byte
 	Function() string
