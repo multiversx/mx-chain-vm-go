f101e38bf10879771f318cef2a33254ef1a15607
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-03-06 11:04:56 +0200

    Sketch support for Linux/ARM64.

--- wasmer2/bridge2.go
+++ wasmer2/bridge2.go
@@ -2,6 +2,7 @@ package wasmer2
 
 // #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
 // #cgo linux,amd64 LDFLAGS:-lvmexeccapi
+// #cgo linux,arm64 LDFLAGS:-lvmexeccapi_arm
 // #cgo darwin,amd64 LDFLAGS:-lvmexeccapi
 // #cgo darwin,arm64 LDFLAGS:-lvmexeccapi_arm
 // #include "./libvmexeccapi.h"
