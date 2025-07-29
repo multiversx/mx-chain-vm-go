588e79a1c408d244c918b3a644991af9d58c2e36
Laurentiu Ciobanu <laurentiu.ciobanu@multiversx.com>
2025-07-29 22:54:41 +0300

    fixed asyncParams

--- vmhost/contexts/asyncParams.go
+++ vmhost/contexts/asyncParams.go
@@ -76,6 +76,122 @@ func createDataFromAsyncParams(
 	return callData.ToBytes(), nil
 }
 
+/*
+		Called when a SCR for a callback is created outside the VM
+		(by createAsyncCallBackSCRFromVMOutput())
+		This is the case
+		A)	after an async call executed following a builtin function call,
+		B)	other cases where processing the output trasnfers of a VMOutput did
+			not produce a SCR of type AsynchronousCallBack
+	    TODO(check): function not used?
+*/
+func AppendAsyncArgumentsToCallbackCallData(
+	hasher crypto.Hasher,
+	data []byte,
+	asyncArguments *vmcommon.AsyncArguments,
+	parseArgumentsFunc func(data string) ([][]byte, error)) ([]byte, error) {
+
+	return appendAsyncParamsToCallData(
+		CreateCallbackAsyncParams(hasher, asyncArguments),
+		data,
+		false,
+		parseArgumentsFunc)
+}
+
+/*
+		Called when a SCR is created from VMOutput in order to recompose
+		async data and call data into a transfer data ready for the SCR
+		(by preprocessOutTransferToSCR())
+	    TODO(check): function not used?
+*/
+func AppendTransferAsyncDataToCallData(
+	callData []byte,
+	asyncData []byte,
+	parseArgumentsFunc func(data string) ([][]byte, error)) ([]byte, error) {
+
+	var asyncParams [][]byte
+	if asyncData != nil {
+		asyncParams, _ = parseArgumentsFunc(string(asyncData))
+		// string start with a @ so first parsed argument will be empty always
+		asyncParams = asyncParams[1:]
+	} else {
+		return callData, nil
+	}
+
+	return appendAsyncParamsToCallData(
+		asyncParams,
+		callData,
+		true,
+		parseArgumentsFunc)
+}
+
+func appendAsyncParamsToCallData(
+	asyncParams [][]byte,
+	data []byte,
+	hasFunction bool,
+	parseArgumentsFunc func(data string) ([][]byte, error)) ([]byte, error) {
+
+	if data == nil {
+		return nil, nil
+	}
+
+	args, err := parseArgumentsFunc(string(data))
+	if err != nil {
+		return nil, err
+	}
+
+	var functionName string
+	if hasFunction {
+		functionName = string(args[0])
+	}
+
+	// check if there is only one argument and that is 0
+	if len(args) != 0 {
+		args = args[1:]
+	}
+
+	callData := txDataBuilder.NewBuilder()
+
+	if functionName != "" {
+		callData.Func(functionName)
+	}
+
+	if len(args) != 0 {
+		for _, arg := range args {
+			callData.Bytes(arg)
+		}
+	} else {
+		if !hasFunction {
+			callData.Bytes([]byte{})
+		}
+	}
+
+	for _, asyncParam := range asyncParams {
+		callData.Bytes(asyncParam)
+	}
+
+	return callData.ToBytes(), nil
+}
+
+/*
+Used by when a callback SCR is created
+ 1. after a failure of an async call
+    Async data is extracted (by extractAsyncCallParamsFromTxData()) and then
+    reappended to the new SCR's callback data (by reapendAsyncParamsToTxData())
+ 2. from the last transfer (see useLastTransferAsAsyncCallBackWhenNeeded())
+*/
+func CreateCallbackAsyncParams(hasher crypto.Hasher, asyncParams *vmcommon.AsyncArguments) [][]byte {
+	if asyncParams == nil {
+		return nil
+	}
+	newAsyncParams := make([][]byte, 4)
+	newAsyncParams[0] = GenerateNewCallID(hasher, asyncParams.CallID, []byte{0})
+	newAsyncParams[1] = asyncParams.CallID
+	newAsyncParams[2] = asyncParams.CallerCallID
+	newAsyncParams[3] = []byte{0}
+	return newAsyncParams
+}
+
 // GenerateNewCallID will generate a new call ID as byte slice
 func GenerateNewCallID(hasher crypto.Hasher, parentCallID []byte, suffix []byte) []byte {
 	newCallID := append(parentCallID, suffix...)
